a7644a7f55de0d1fc74c3764c8224a2ddd652b29
Pierre Courtois <pierre.courtois@epita.fr>
2021-11-27 17:32:10 +0100

    [ADD] Added infinity scrolling

--- src/catalog-view.ts
+++ src/catalog-view.ts
@@ -10,6 +10,9 @@ import {
 
 export default class CatalogView extends View {
     meshList: {mesh: Mesh}[] = []
+    spawnRange = 2
+    itemMargin = 1
+
 
     public initialize() {
         super.initialize();
@@ -28,8 +31,8 @@ export default class CatalogView extends View {
 
         window.addEventListener('wheel', (event) => {
             event.preventDefault()
-            this.updateMeshPositions(event.deltaY * 0.005)
-        })
+            this.updateMeshPositions(event.deltaY * 0.001)
+        }, { passive: false })
     }
 
     public initMeshes() {
@@ -46,24 +49,50 @@ export default class CatalogView extends View {
         const mars = new Mesh(new OctahedronGeometry(0.03, 4), new MeshLambertMaterial());
         mars.material.color.set('#9B7653');
         this.meshList.push({mesh: mars})
+
+        const earth2 = new Mesh(new OctahedronGeometry(0.05, 1), new MeshBasicMaterial());
+        earth2.material.color.set('#20FF00');
+        earth2.scale.set(0.5, 0.5, 0.5);
+        this.meshList.push({mesh: earth2})
+
+        const earth3 = new Mesh(new OctahedronGeometry(0.05, 1), new MeshBasicMaterial());
+        earth3.material.color.set('#FF0020');
+        earth3.scale.set(0.5, 0.5, 0.5);
+        this.meshList.push({mesh: earth3})
     }
 
     public displayMeshes() {
         for (let i = 0; i < this.meshList.length; i++) {
-            this._scene.add(this.meshList[i].mesh)
+            let currentMesh = this.meshList[i].mesh
+
+            if (- this.spawnRange <= currentMesh.position.x && currentMesh.position.x <= this.spawnRange)
+                this._scene.add(currentMesh)
+            else
+                this._scene.remove(currentMesh)
         }
     }
 
     public setMeshPositions() {
-        for (let i = 0; i < this.meshList.length; i++) {
-            this.meshList[i].mesh.position.set(i, 0, 0)
+        for (let i = 0; i < this.meshList.length - 1; i++) {
+            this.meshList[i].mesh.position.set(i * this.itemMargin, 0, 0)
         }
+
+        this.meshList[this.meshList.length - 1].mesh.position.set(-1 * this.itemMargin, 0, 0)
     }
 
     public updateMeshPositions(value: number) {
+        let limit = (this.meshList.length - 2) * this.itemMargin
         for (let i = 0; i < this.meshList.length; i++) {
-            this.meshList[i].mesh.position.x += value;
+            let newPos = this.meshList[i].mesh.position.x - value
+
+            if (newPos >= limit)
+                newPos = -this.itemMargin * 2
+            else if (newPos <= -limit)
+                newPos = this.itemMargin * 2
+
+            this.meshList[i].mesh.position.x = newPos
         }
+        this.displayMeshes()
     }
 
     public destroy() {
