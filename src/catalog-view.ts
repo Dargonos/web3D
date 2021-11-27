6b0c7c860bfdf839c282d225d70afbd9b97da8ba
Pierre Courtois <pierre.courtois@epita.fr>
2021-11-27 16:32:33 +0100

    [ADD] Added basic scrolling between meshs

--- src/catalog-view.ts
+++ src/catalog-view.ts
@@ -1,19 +1,69 @@
 import {View} from "./view";
-import {CircleGeometry, Mesh, MeshBasicMaterial, PointLight} from "three";
+import {
+    CircleGeometry,
+    DirectionalLight,
+    Mesh,
+    MeshBasicMaterial,
+    MeshLambertMaterial,
+    OctahedronGeometry,
+} from "three";
 
 export default class CatalogView extends View {
+    meshList: {mesh: Mesh}[] = []
+
     public initialize() {
         super.initialize();
 
+        this.initMeshes()
+
+        this.setMeshPositions()
+
+        this.displayMeshes()
+
+        const dirLight = new DirectionalLight(0xffc0cb, 1);
+        dirLight.add(dirLight.target);
+        dirLight.position.set(0, 0, 0);
+        dirLight.target.position.set(8, -8, -12);
+        dirLight.castShadow = true;
+
+        window.addEventListener('wheel', (event) => {
+            event.preventDefault()
+            this.updateMeshPositions(event.deltaY * 0.005)
+        })
+    }
+
+    public initMeshes() {
         const sun = new Mesh(new CircleGeometry(0.15, 32), new MeshBasicMaterial());
         sun.material.color.set('#FFFF00');
         sun.scale.set(0.5, 0.5, 0.5);
-        this._scene.add(sun);
+        this.meshList.push({mesh: sun})
 
-        const sunLight = new PointLight('#FFFFFF', 10, 0);
-        sunLight.decay = 0;
-        sunLight.position.set(0,0,0);
-        this._scene.add(sunLight);
+        const earth = new Mesh(new OctahedronGeometry(0.05, 1), new MeshBasicMaterial());
+        earth.material.color.set('#0020FF');
+        earth.scale.set(0.5, 0.5, 0.5);
+        this.meshList.push({mesh: earth})
+
+        const mars = new Mesh(new OctahedronGeometry(0.03, 4), new MeshLambertMaterial());
+        mars.material.color.set('#9B7653');
+        this.meshList.push({mesh: mars})
+    }
+
+    public displayMeshes() {
+        for (let i = 0; i < this.meshList.length; i++) {
+            this._scene.add(this.meshList[i].mesh)
+        }
+    }
+
+    public setMeshPositions() {
+        for (let i = 0; i < this.meshList.length; i++) {
+            this.meshList[i].mesh.position.set(i, 0, 0)
+        }
+    }
+
+    public updateMeshPositions(value: number) {
+        for (let i = 0; i < this.meshList.length; i++) {
+            this.meshList[i].mesh.position.x += value;
+        }
     }
 
     public destroy() {
