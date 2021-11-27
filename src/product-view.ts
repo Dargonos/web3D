cbc0f23572b28f74c81fe8acf51c963df0c6a6ac
Ariane DELTOUR <ariane.deltour@epita.fr>
2021-11-27 17:27:06 +0100

    [ADD] push light

--- src/product-view.ts
+++ src/product-view.ts
@@ -1,6 +1,8 @@
 import {View} from "./view";
 import {
-    PMREMGenerator, Raycaster,
+    CameraHelper,
+    DirectionalLight, DirectionalLightHelper, Mesh, PlaneBufferGeometry,
+    PMREMGenerator, Raycaster, ShadowMaterial,
     UnsignedByteType, Vector2,
     WebGLRenderer
 } from "three";
@@ -14,12 +16,39 @@ export default class ProductView extends View {
     controls: OrbitControls;
     raycaster: Raycaster;
     mouse: Vector2;
+    light: DirectionalLight;
+
+    shadowPlaneGeometry: PlaneBufferGeometry;
+    shadowPlaneMaterial: ShadowMaterial;
+    shadowPlaneMesh: Mesh;
 
     constructor(renderer: WebGLRenderer, gltfPath: string) {
         super(renderer);
 
         this.raycaster = new Raycaster();
         this.mouse = new Vector2(0,0);
+
+        //TODO position light
+        this.light = new DirectionalLight(0xffffff);
+        this.light.add(this.light.target);
+        this.light.position.set(0, 0, 0);
+        this.light.target.position.set(10, -10, -12);
+
+        this.light.shadow.mapSize.set(1024,1024);
+        this.light.shadow.camera.near = -5;
+        this.light.shadow.camera.far = 8;
+        this.light.castShadow = true;
+
+        this.shadowPlaneGeometry = new PlaneBufferGeometry(100,100);
+        this.shadowPlaneMaterial = new ShadowMaterial();
+        this.shadowPlaneMesh = new Mesh(this.shadowPlaneGeometry, this.shadowPlaneMaterial);
+        this.shadowPlaneMesh.position.set(0,-0.55,0);
+        this.shadowPlaneMesh.rotation.x =  - Math.PI / 2;
+        this.shadowPlaneMesh.receiveShadow = true;
+
+        const cameraHelper = new CameraHelper(this.light.shadow.camera);
+        const helper = new DirectionalLightHelper(this.light);
+        this._scene.add(helper, cameraHelper);
 
         //TODO remove
         gltfPath = 'assets/models/LittlestTokyo.glb';
@@ -35,11 +64,13 @@ export default class ProductView extends View {
                 const model = gltf.scene;
                 model.position.set(0,0,0)
                 model.scale.set( 0.01, 0.01, 0.01 )
+                model.castShadow = true
                 this._scene.add(model)
             },
             undefined,
             (err) => console.error(err)
         );
+
 
         this.controls = new OrbitControls(this._cam, this._renderer.domElement) as OrbitControls;
 
@@ -52,6 +83,13 @@ export default class ProductView extends View {
                 this._scene.environment = target.texture;
                 this._scene.background = target.texture;
             });
+
+        //TODO add edition color panel
+        this._scene.add(this.light, this.shadowPlaneMesh)
+
+
+        //TODO arrow left tight to change mesh
+
     }
 
     public initialize() {
@@ -74,6 +112,9 @@ export default class ProductView extends View {
     public destroy() {
         super.destroy();
 
+        this.light.dispose()
+        this.shadowPlaneGeometry.dispose()
+        this.shadowPlaneMaterial.dispose()
     }
 
     public update(delta: number, elapsed: number) {
