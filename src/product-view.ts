1fadfecb612ee727d7a73a8cacdf7014b847dbdc
Pierre Courtois <pierre.courtois@epita.fr>
2021-11-28 01:46:11 +0100

    [ADD] Add color Picker

--- src/product-view.ts
+++ src/product-view.ts
@@ -1,9 +1,10 @@
 import {View} from "./view";
 import {
-    CameraHelper,
-    DirectionalLight, DirectionalLightHelper, Mesh, PlaneBufferGeometry,
-    PMREMGenerator, Raycaster, ShadowMaterial,
-    UnsignedByteType, Vector2,
+    Color,
+    DirectionalLight, Mesh, MeshPhysicalMaterial, PlaneBufferGeometry,
+    PMREMGenerator, Raycaster, ShadowMaterial, SphereBufferGeometry,
+    UnsignedByteType,
+    Vector2,
     WebGLRenderer
 } from "three";
 import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader";
@@ -21,6 +22,16 @@ export default class ProductView extends View {
     shadowPlaneGeometry: PlaneBufferGeometry;
     shadowPlaneMaterial: ShadowMaterial;
     shadowPlaneMesh: Mesh;
+
+    colors : Color[] = [
+        new Color("rgb(255,0,0)"),
+        new Color("rgb(242,122,16)"),
+        new Color( "rgb(255,235,0)"),
+        new Color("rgb(0,255,0)"),
+        new Color("rgb(0,0,255)"),
+        new Color("rgb(94,15,215)"),
+        new Color( "rgb(224,109,231)")];
+    colorPickerGeometry: SphereBufferGeometry;
 
     constructor(renderer: WebGLRenderer, gltfPath: string) {
         super(renderer);
@@ -46,9 +57,15 @@ export default class ProductView extends View {
         this.shadowPlaneMesh.rotation.x =  - Math.PI / 2;
         this.shadowPlaneMesh.receiveShadow = true;
 
-        const cameraHelper = new CameraHelper(this.light.shadow.camera);
-        const helper = new DirectionalLightHelper(this.light);
-        this._scene.add(helper, cameraHelper);
+        this.colorPickerGeometry = new SphereBufferGeometry(0.4)
+
+        for (let idx = 0; idx < 7; idx++) {
+            let mesh = new Mesh(this.colorPickerGeometry, new MeshPhysicalMaterial())
+            mesh.position.set(5, idx, 0)
+            mesh.material.color.set(this.colors[idx])
+            mesh.name = "Picker"
+            this._scene.add(mesh)
+        }
 
         //TODO remove
         gltfPath = 'assets/models/LittlestTokyo.glb';
@@ -65,12 +82,19 @@ export default class ProductView extends View {
                 model.position.set(0,0,0)
                 model.scale.set( 0.01, 0.01, 0.01 )
                 model.castShadow = true
+
+                model.traverse((object) => {
+                    if (object instanceof Mesh) {
+                        object.material.color = this.colors[0];
+                    }
+                });
+
                 this._scene.add(model)
             },
             undefined,
             (err) => console.error(err)
         );
-
+        //this.traverseScene(this.colors[0])
 
         this.controls = new OrbitControls(this._cam, this._renderer.domElement) as OrbitControls;
 
@@ -95,15 +119,19 @@ export default class ProductView extends View {
     public initialize() {
         super.initialize()
 
-        this._renderer.domElement.addEventListener( 'dblclick', (event) => {
-            this.mouse.x = ( event.clientX / window.innerWidth ) * 2.0 - 1.0;
-            this.mouse.y = ( event.clientY / window.innerHeight ) * 2.0 - 1.0;
+        this._renderer.domElement.addEventListener( 'click', (event) => {
+            this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            this.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
-            this.raycaster.setFromCamera(this.mouse, this._cam);
-            const intersection = this.raycaster.intersectObject(this._scene, true);
+            this.raycaster.setFromCamera(this.mouse, this._cam)
+            const intersection = this.raycaster.intersectObject(this._scene, true)
             if (intersection && intersection.length > 0) {
-                //TODO zoom
+                if (intersection[0].object instanceof Mesh && intersection[0].object.name == "Picker") {
+                    const newColor = intersection[0].object.material.color;
+                    this.traverseScene(newColor);
+                }
             }
+
         });
 
         this._gui.hide()
@@ -115,11 +143,20 @@ export default class ProductView extends View {
         this.light.dispose()
         this.shadowPlaneGeometry.dispose()
         this.shadowPlaneMaterial.dispose()
+
+        this.colorPickerGeometry.dispose()
     }
 
     public update(delta: number, elapsed: number) {
 
-
         this.controls.update();
+    }
+
+    public traverseScene(color: Color) {
+        this._scene.traverse((object) => {
+            if (object instanceof Mesh && object.name != "Picker") {
+                object.material.color = color;
+            }
+        });
     }
 }
